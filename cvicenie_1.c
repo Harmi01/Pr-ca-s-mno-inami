1cedd76f9f2d488a74b37511818f323bc1766aff
Harmi <harmaniakadam@gmail.com>
2023-03-23 20:26:20 +0100

    zjednotenie_2

--- cvicenie_1.c
+++ cvicenie_1.c
@@ -14,28 +14,27 @@ int prienik_pocet(int M[],int K[]){
     return n;
 }
 
-void prienik(int M[],int K[],int S[]){
+void prienik(int M[],int K[],int P[]){
     int tmp=0;
     for(int i=0; i<n_M; i++){
         for(int j=0; j<n_K; j++){
             if(M[i]==K[j]){
-                S[tmp]=K[j];
+                P[tmp]=K[j];
                 tmp++;
             }
         }
     }
 }
 
-void zjednotenie(int M[],int K[],int P[],int S[]){
+void zjednotenie(int M[],int K[],int Z[],int P[]){
     
     for(int i=0; i<n_M; i++){
-        P[i]=M[i];
+        Z[i]=M[i];
     }
-    for(int j=0; j<n_K; j++){
-        if(K[j]==S[0] || K[j]==S[1])
+    for(int j=0,z=5; j<n_K; j++,z++){
+        if(K[j]==P[0] || K[j]==P[1])
             continue;
-        else
-            P[j+6]=K[j];
+        Z[z]=K[j];
         
     }
 }
@@ -43,19 +42,19 @@ void zjednotenie(int M[],int K[],int P[],int S[]){
 
 int main(){
     int M[n_M]={3,6,7,10,18}, K[n_K]={2,8,10,11,15,18};
-    int n_S=prienik_pocet(M,K);
-    int S[n_S];
-    int P[n_M+n_K];
-    prienik(M,K,S);
-
-    for(int i=0; i<n_S; i++){
-        printf("%d ",S[i]);
+    int n_P=prienik_pocet(M,K);
+    int P[n_P];
+    int Z[(n_M+n_K)-n_P];
+    
+    prienik(M,K,P);
+    zjednotenie(M,K,Z,P);
+    
+    for(int i=0; i<n_P; i++){
+        printf("%d ",P[i]);
     }
     printf("\n");
-
-    zjednotenie(M,K,P,S);
-    for(int j=0; j<n_M+n_K; j++){
-        printf("%d ",P[j]);
+    for(int j=0; j<(n_M+n_K)-n_P; j++){
+        printf("%d ",Z[j]);
     }
 
 }
